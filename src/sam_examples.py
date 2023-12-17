74591dd3d1c511f3e8dd3d667035909037d7e869
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-12-17 18:07:32 +0300

    feat: 🖼️  sam example box and points are added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

---
+++ src/sam_examples.py
@@ -0,0 +1,11 @@
+BOX_EXAMPLES = [
+    ["https://media.roboflow.com/efficient-sam/corgi.jpg", 801, 510, 1782, 993],
+    ["https://media.roboflow.com/efficient-sam/horses.jpg", 814, 696, 1523, 1183],
+    ["https://media.roboflow.com/efficient-sam/bears.jpg", 653, 874, 1173, 1229],
+]
+
+POINT_EXAMPLES = [
+    ["https://media.roboflow.com/efficient-sam/corgi.jpg", 1291, 751],
+    ["https://media.roboflow.com/efficient-sam/horses.jpg", 1168, 939],
+    ["https://media.roboflow.com/efficient-sam/bears.jpg", 913, 1051],
+]
