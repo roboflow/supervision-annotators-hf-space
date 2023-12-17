418543672cda9703f4e4c996103646b855839d5a
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-12-17 19:58:45 +0300

    feat: ✨ SAM box Inference and progressbar for downloading sam models
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- src/sam/sam_inference.py
+++ src/sam/sam_inference.py
@@ -1,3 +1,4 @@
+import gradio as gr
 import numpy as np
 import supervision as sv
 import torch
@@ -17,16 +18,10 @@ class SamInference:
         self.prompt_color = sv.Color.from_hex("#D3D3D3")
         self.mask_color = sv.Color.from_hex("#FF0000")
 
-    def get_sam_model(self):
-        return self.sam_model
-
-    def get_sam_processor(self):
-        return self.sam_processor
-
-    def set_sam_model(self, model: str):
+    def set_sam_model(self, model: str, progress=gr.Progress(track_tqdm=True)):
         self.sam_model = SamModel.from_pretrained(model).to(DEVICE)
 
-    def set_sam_processor(self, processor: str):
+    def set_sam_processor(self, processor: str, progress=gr.Progress(track_tqdm=True)):
         self.sam_processor = SamProcessor.from_pretrained(processor)
 
     def annotate_image_with_point_prompt_result(
@@ -63,4 +58,58 @@ class SamInference:
         detections = sv.Detections(xyxy=sv.mask_to_xyxy(masks=mask), mask=mask)
         return self.annotate_image_with_point_prompt_result(
             image=image, detections=detections, x=x, y=y
+        )
+
+    def annotate_image_with_box_prompt_result(
+        self,
+        image: np.ndarray,
+        detections: sv.Detections,
+        x_min: int,
+        y_min: int,
+        x_max: int,
+        y_max: int,
+    ) -> np.ndarray:
+        h, w, _ = image.shape
+        bgr_image = image[:, :, ::-1]
+        annotated_bgr_image = self.mask_annotator.annotate(
+            scene=bgr_image, detections=detections
+        )
+        annotated_bgr_image = sv.draw_rectangle(
+            scene=annotated_bgr_image,
+            rect=sv.Rect(
+                x=x_min,
+                y=y_min,
+                width=int(x_max - x_min),
+                height=int(y_max - y_min),
+            ),
+            color=self.prompt_color,
+            thickness=sv.calculate_dynamic_line_thickness(resolution_wh=(w, h)),
+        )
+        return annotated_bgr_image[:, :, ::-1]
+
+    def sam_box_inference(
+        self, image: np.ndarray, x_min: int, y_min: int, x_max: int, y_max: int
+    ) -> np.ndarray:
+        input_boxes = [[[x_min, y_min, x_max, y_max]]]
+        inputs = self.sam_processor(
+            Image.fromarray(image), input_boxes=[input_boxes], return_tensors="pt"
+        ).to(DEVICE)
+
+        with torch.no_grad():
+            outputs = self.sam_model(**inputs)
+
+        mask = self.sam_processor.image_processor.post_process_masks(
+            outputs.pred_masks.cpu(),
+            inputs["original_sizes"].cpu(),
+            inputs["reshaped_input_sizes"].cpu(),
+        )[0][0][0].numpy()
+        mask = mask[np.newaxis, ...]
+        detections = sv.Detections(xyxy=sv.mask_to_xyxy(masks=mask), mask=mask)
+        return self.annotate_image_with_box_prompt_result(
+            image=image,
+            detections=detections,
+            x_max=x_max,
+            x_min=x_min,
+            y_max=y_max,
+            y_min=y_min,
         )
--- src/sam_tab.py
+++ src/sam_tab.py
@@ -1,6 +1,6 @@
 import gradio as gr
 
-from src.sam.sam_examples import POINT_EXAMPLES
+from src.sam.sam_examples import BOX_EXAMPLES, POINT_EXAMPLES
 from src.sam.sam_inference import SamInference
 
 # gradio Segmentation Anything Demo
@@ -8,19 +8,10 @@ from src.sam.sam_inference import SamInference
 sam_inference = SamInference()
 
 
-def sam_segmentation(model, image):
-    sam_inference.set_sam_model(model)
-    sam_inference.set_sam_processor(model)
-
-
-# SAM Mode enum for point and box input and defualt to point
-class SamMode:
-    POINT = "Point"
-    BOX = "Box"
-
-
-# SAM Mode enum default to point
-sam_mode: SamMode = SamMode.POINT
+def sam_segmentation(model, progress=gr.Progress(track_tqdm=True)):
+    sam_inference.set_sam_model(model, progress=gr.Progress(track_tqdm=True))
+    sam_inference.set_sam_processor(model, progress=gr.Progress(track_tqdm=True))
+    return "Model is ready"
 
 
 box_input_image = gr.Image(label="SAM Box Input")
@@ -35,6 +26,7 @@ x_number = gr.Number(label="x")
 y_number = gr.Number(label="y")
 box_annotate_button = gr.Button(value="Annotate it!", variant="primary")
 point_annotate_button = gr.Button(value="Annotate it!", variant="primary")
+download_status_label = gr.Label("Model is ready", show_label=False)
 
 
 def box_and_point_menu(gr: gr):
@@ -54,6 +46,18 @@ def box_and_point_menu(gr: gr):
                         y_min_number.render()
                         x_max_number.render()
                         y_max_number.render()
+        gr.Examples(
+            fn=sam_inference.sam_box_inference,
+            examples=BOX_EXAMPLES,
+            inputs=[
+                box_input_image,
+                x_min_number,
+                y_min_number,
+                x_max_number,
+                y_max_number,
+            ],
+            outputs=[box_output_image],
+        )
     with gr.TabItem("Point Selection"):
         with gr.Row():
             with gr.Column():
@@ -76,17 +80,20 @@ def box_and_point_menu(gr: gr):
 
 
 def sam_models(gr: gr):
-    models = gr.Dropdown(
-        [
-            "facebook/sam-vit-base",
-            "facebook/sam-vit-large",
-            "facebook/sam-vit-huge",
-        ],
-        type="value",
-        value="facebook/sam-vit-huge",
-        label="Select SAM Model:",
-    )
-    return models
+    with gr.Row():
+        models = gr.Dropdown(
+            [
+                "facebook/sam-vit-base",
+                "facebook/sam-vit-large",
+                "facebook/sam-vit-huge",
+            ],
+            type="value",
+            value="facebook/sam-vit-huge",
+            label="Select SAM Model:",
+            min_width=900,
+        )
+        download_status_label.render()
+        return models
 
 
 def sam_annotator_tab(gr: gr):
@@ -94,11 +101,24 @@ def sam_annotator_tab(gr: gr):
     with gr.Tab("Image Annotator(SAM)"):
         model = sam_models(gr)
         box_and_point_menu(gr)
-        box_annotate_button.click(
-            fn=sam_segmentation, inputs=[model, model], outputs=[box_output_image]
+        model.select(
+            fn=sam_segmentation, inputs=[model], outputs=[download_status_label]
         )
+
         point_annotate_button.click(
             fn=sam_inference.sam_point_inference,
             inputs=[point_input_image, x_number, y_number],
             outputs=[point_output_image],
+        )
+
+        box_annotate_button.click(
+            fn=sam_inference.sam_box_inference,
+            inputs=[
+                box_input_image,
+                x_min_number,
+                y_min_number,
+                x_max_number,
+                y_max_number,
+            ],
+            outputs=[box_output_image],
         )
