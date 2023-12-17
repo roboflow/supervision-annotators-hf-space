5e8c5b71c6737e161d0dd5ab94a0d34f2b23ba39
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2023-12-17 10:58:46 +0300

    feat(tabs): ✨ initial sam and video tabs added
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

---
+++ src/sam_tab.py
@@ -0,0 +1,9 @@
+import gradio as gr
+
+# gradio Segmentation Anything Demo
+
+
+def sam_annotator_tab(gr: gr):
+    """Run the SAM menu."""
+    with gr.Tab("Image Annotator(SAM)"):
+        gr.HTML("Coming soon...")
---
+++ src/video_tab.py
@@ -0,0 +1,6 @@
+import gradio as gr
+
+def video_annotator_tab(gr: gr):
+    """Run the SAM menu."""
+    with gr.Tab("Image Annotator(SAM)"):
+        gr.HTML("Coming soon...")
