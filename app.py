fe6b424fbaefce2afd95d6202aed234012079732
Onuralp SEZER <thunderbirdtr@fedoraproject.org>
2024-01-13 04:05:06 +0300

    feat: ✨ 'SPACE_REPO_NAME' env check added for enable/disable debug mode
    Signed-off-by: Onuralp SEZER <thunderbirdtr@fedoraproject.org>

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-
+import os
 from src.annotator_tab import annotator_tab
 from src.gradio_desc import gradio_desc
 from src.sam_tab import sam_annotator_tab
@@ -21,7 +21,12 @@ with gr.Blocks(theme=purple_theme) as app:
 
 
 if __name__ == "__main__":
-    print("Starting app...")
+    # Check is SPACE_REPO_NAME env var is set
+    if "SPACE_REPO_NAME" in os.environ:
+        print("Starting app in space repo...")
+        app.launch(debug=False)
+
+    print("Starting app in outside space repo... (Debug mode))")
     print("Dark theme is available at: http://localhost:7860/?__theme=dark")
-    # app.launch(debug=False, server_name="0.0.0.0")  # for local network
-    app.launch(debug=False)
+    app.launch(debug=True, server_name="0.0.0.0")  # for local network
+
